db0d00f1a4fa8815fde42d5e3f493b342395c8d5
ilbrigz <ilogirb23@yahoo.com>
2019-07-04 18:37:41 +0800

    second trial after error

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,5 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./Header"
+import Header from "./Header.js"
 import "./layout.css"
 
 const Layout = ({ children, offsetY }) => {
