bdc1e8bcf8979ecd24617b1dc3e22d4cacd09e9b
ilbrigz <ilogirb23@yahoo.com>
2019-07-10 06:29:14 +0800

    .

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,9 +10,10 @@ const StyledCards = styled.div`
   justify-items: center;
   display: flex;
   flex-direction: column;
-  align-items: start;
   margin-top: 3rem;
+  align-items: center;
   @media (min-width: 570px) {
+    align-items: start;
     display: grid;
     grid-gap: 3rem;
     grid-template-columns: repeat(2, 1fr);
